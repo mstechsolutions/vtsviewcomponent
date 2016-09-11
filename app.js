32b19d0fe544f7f16c9700443a37b23d5b5935fd
Veerabathiran, Mahesh <mahesh.veerabathiran@capitalone.com>
2016-09-11 14:29:55 -0400

    add order enabled

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ app.controller('mainCtrl', ['$scope','$http', function($scope, $http) {
   $scope.isPickupContactSameAsCC = true;
 
   $scope.gridHeaders = [
-    "Truck Id",
+    // "Truck Id",
     "Truck name",
     "Customer name",
     "Customer phone",
-    "Pickup state",
-    "Pickup zipCode",
-    "Dropoff state",
-    "Dropoff zipCode",
+    "Order date",
+    "Pickup date",
+    "Pickup location",
+    // "Pickup state",
+    // "Pickup zipCode",
+    "Dropoff date",
+    "Dropoff location",
+    // "Dropoff state",
+    // "Dropoff zipCode",
     "Payment mode",
     "Expected miles",
     "Expense",
@@ -47,13 +52,54 @@ app.controller('mainCtrl', ['$scope','$http', function($scope, $http) {
 
   $scope.response = null;
   $scope.addOrderSubmit = function(){
-    
+  
     var customerInfo = new Object();
     customerInfo.name = $scope.customerName;
 
     var order = new Object();
+    order.truckId=$scope.selectedTruck.id;
     order.truckName=$scope.selectedTruck.name;
+    
+    var pickupContactInfo = null;
+    var customerInfo = new Object();
+    customerInfo.name = $scope.customerName;
+    customerInfo.phone = $scope.customerContact;
+    customerInfo.state = $scope.customerState;
+    customerInfo.zipCode = $scope.customerZipCode;
+    customerInfo.city = $scope.customerCity;
     order.customerInfo = customerInfo;
+
+
+    if($scope.isPickupContactSameAsCC)
+    {
+      pickupContactInfo=customerInfo;
+    }
+    else
+    {
+      pickupContactInfo = new Object();
+      pickupContactInfo.state = $scope.pickupState;
+      pickupContactInfo.zipCode = $scope.pickupZipCode;
+      pickupContactInfo.city = $scope.pickupCity;
+      
+    }  
+    order.pickupContactInfo = pickupContactInfo;
+    alert(pickupContactInfo.state + "," + pickupContactInfo.zipCode)
+
+    var dropoffContactInfo = new Object();
+    dropoffContactInfo.state = $scope.dropoffState;
+    dropoffContactInfo.zipCode = $scope.dropoffZipCode;
+    dropoffContactInfo.city = $scope.dropoffCity;
+    order.dropoffContactInfo = dropoffContactInfo;
+
+    // order.orderDate = $scope.orderDate;
+
+    order.orderDate = new Date();
+    order.pickupDate = $scope.pickupDate;
+    order.dropoffDate = $scope.dropoffDate;
+    order.paymentMode = $scope.selectedPaymentMode;
+    order.expectedMiles = $scope.expectedMiles;
+    order.cost = $scope.cost;
+    order.expense = $scope.expense;
     $scope.response.push(order)
 
     //Hide the modal if all input given is valid
